876acb648cd2aae0d29ac993c81fcf5c57f78afa
Andreu Vila <u1933593@campus.udg.edu>
2018-11-05 18:27:44 +0100

    Exercici 3 - Andreu Vila
    Exercici 3 Andreu Vila

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -10,11 +10,27 @@ import mockit.Mocked;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestAccountService {
-
+public class TestAccountService
+{
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+    Account l_envia = new Account("Enviador",2000);
+    Account l_rep = new Account("Andreu",1000);
+    new Expectations()
+    {{
+      am.findAccount("Enviador"); result = l_envia;
+      am.findAccount("Andreu"); result = l_rep;
+    }};
+
+    as.setAccountManager(am);
+    as.transfer("Enviador","Andreu",500);
+
+    new Verifications()
+    {{
+      am.updateAccount(l_envia);
+      am.updateAccount(l_rep);
+    }};
   }
+
 }
